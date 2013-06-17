8584fde785605b26d8d310f57acb99b195e9f977
mike.weib@gmail.com <mike.weib@gmail.com>
2013-06-17 11:45:12 +0800

    添加平台区分.

--- sample/proj.android/src/com/talkingdata/cocos2dx/TalkingDataCocos2dx.java
+++ sample/proj.android/src/com/talkingdata/cocos2dx/TalkingDataCocos2dx.java
@@ -12,22 +12,23 @@ public class TalkingDataCocos2dx extends Cocos2dxActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TalkingDataGA.sPlatformType = TalkingDataGA.PLATFORM_TYPE_COCOS2DX;
         TalkingDataGA.DEBUG = true;
         // 代码初始化, 也可以在AndroidManifest.xml中声明.
-//        TalkingDataGA.init(this, "2B002F9CD724EB09730AF32CB4D909C3", "test");
+        // TalkingDataGA.init(this, "2B002F9CD724EB09730AF32CB4D909C3", "test");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d("TalkingDataCocos2dx","onResume");
+        Log.d("TalkingDataCocos2dx", "onResume");
         TalkingDataGA.onResume(this);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d("TalkingDataCocos2dx","onPause");
+        Log.d("TalkingDataCocos2dx", "onPause");
         TalkingDataGA.onPause(this);
     }
 
